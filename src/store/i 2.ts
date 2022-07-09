2f3203986caa40ed330af95745e71398d9b79479
Maksim Makurin <antidod89@gmail.com>
2022-07-09 21:15:58 +0300

    lesson 8

--- src/store/i 2.ts
+++
@@ -1,7 +0,0 @@
-import { ITodosState } from './pages/TodosPage/interfaces';
-import { IUsersState } from './pages/UsersPage/interfaces';
-
-export interface IStore {
-  users: IUsersState;
-  todos: ITodosState;
-}
